4aef6f91db103001b87e31b5748c549ac38810ce
mominosin <shingo.suzuki@mixi.co.jp>
2022-08-25 19:12:20 +0900

    test go

--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module github.com/mominosin/renovate-test
 
 go 1.17
 
-require github.com/spf13/cobra v1.1.3
+require github.com/tidwall/gjson v1.6.5
 
 require (
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tidwall/match v1.0.3 // indirect
+	github.com/tidwall/pretty v1.0.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,25 +1,10 @@
 package main
 
-import (
-    "fmt"
-    "github.com/spf13/cobra"
-    "os"
-)
+import "github.com/tidwall/gjson"
 
-var rootCmd = &cobra.Command{
-    Use:   "helloWorldApp",
-    Short: "A brief description of your application",
-    Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-    Run: func(cmd *cobra.Command, args []string) { 
-        fmt.Println("hello world!")
-    },
-}
+const json = `{"name":{"first":"Janet","last":"Prichard"},"age":47}`
 
 func main() {
-	fmt.Println("vim-go")
+	value := gjson.Get(json, "name.last")
+	println(value.String())
 }
