a7ee34cabd81f44fb1750fbdbe50583330c08593
mominosin <shingo.suzuki@mixi.co.jp>
2022-08-26 10:54:01 +0900

    add node

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var minimatch = require("minimatch")
+var os = require("os")
+var chalk = require("chalk")
+
+console.log("app running")
 
 minimatch("bar.foo", "*.foo") // true!
 minimatch("bar.foo", "*.bar") // false!
