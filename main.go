6e44cf00f1d9f23ba334b4d676589e376029a231
mominosin <shingo.suzuki@mixi.co.jp>
2022-10-19 21:39:29 +0900

    add options

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
-import "github.com/tidwall/gjson"
+import (
+	"github.com/tidwall/gjson"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
 
 const json = `{"name":{"first":"Janet","last":"Prichard"},"age":47}`
 
 func main() {
 	value := gjson.Get(json, "name.last")
 	println(value.String())
+
+	grpc.NewServer()
 }
